02a3d2f0e743eb2d3aea56834616e1f230f2f20e
RuchiSingh7095 <124230934+RuchiSingh7095@users.noreply.github.com>
2024-05-02 10:15:49 +0000

    conversion of operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -22,5 +22,36 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************************************** Operations **************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = " hii"
+let str2 = "ruchi"
+
+let str3 = str1 + " " + str2
+// console.log(str3);
+
+// console.log("1" + 2); //if first value is string it will take next values as string because in this case conversion is depends upon first value 
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
