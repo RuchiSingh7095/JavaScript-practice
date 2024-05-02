b3e625d49460cfd6232f51e87b0877482fa33d57
RuchiSingh7095 <124230934+RuchiSingh7095@users.noreply.github.com>
2024-05-02 08:46:02 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+// console.log(3+3); console.log("Ruchi"); //code readability should be high
+
+let name = "Ruchi"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number -> range of number (2 to power 53)
+// bigint
+// string -> ""
+// boolean -> true/false
+// null -> standalone value (it is object type)
+// undefined
+// symbol -> unique
+
+// object - {}
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
