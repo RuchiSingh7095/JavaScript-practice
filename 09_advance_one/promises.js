e84d6884a68e0db69928bed5e4c517df9e5a4264
RuchiSingh7095 <124230934+RuchiSingh7095@users.noreply.github.com>
2024-06-24 05:38:16 +0000

    promises with fetch()

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -121,8 +121,8 @@ consumePromiseFive();
 async function getAllUser(){
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users')
-        console.log(response);
-        const data = response.json();
+        const data = await response.json();
+        console.log(data);
     } catch (error) {
         console.log(error);
     }
